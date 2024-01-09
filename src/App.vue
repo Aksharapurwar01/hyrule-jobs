f1005711f10a665ab9b244e6456889927e978c65
Akshara Purwar <akshpurwar12@gmail.com>
2024-01-09 12:52:17 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div class="app">
-    <h1>hello</h1>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
+export default Vue.extend({
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+});
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
